c22b1c6d5d5738f95d990d0b6f58649cf8412c99
Jan Vonde <mail@jan-von.de>
2014-09-10 18:17:15 +0200

    make puppet-lint more happy

--- manifests/var.pp
+++ manifests/var.pp
@@ -8,16 +8,16 @@ define systemenv::var (
   validate_re($ensure, '^present|absent$')
   validate_re($varname, '^[a-zA-Z][a-zA-Z0-9_]+')
   validate_re($value, '(^\'.*\'$)|(^\"\.*\"$)')
-  
+
   $target = $systemenv::params::default_env_settings_file
   validate_absolute_path($target)
 
   ini_setting { "${varname}=${value}":
-    ensure => $ensure,
-    path => $target,
-    section => '',
-    setting => $varname,
-    value => $value,
+    ensure            => $ensure,
+    path              => $target,
+    section           => '',
+    setting           => $varname,
+    value             => $value,
     key_val_separator => '=',
   }
 }
